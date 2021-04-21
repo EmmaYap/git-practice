5434498e543e7aab6efb09ccfc3c7b46efcd69f0
EmmaYap <emmanuelyap.20@ichat.sp.edu.sg>
2021-04-21 12:30:37 +0800

    Make sum(x, y) use + operator

--- calculator.js
+++ calculator.js
@@ -1,12 +1,7 @@
 // https://www.geeksforgeeks.org/add-two-numbers-without-using-arithmetic-operators/
-module.exports.sum = (x, y) => {
-    while (y !== 0) {
-        let carry = x & y;
-        x = x ^ y;
-        y = carry << 1;
-    }
-    return x;
-};
+function sum(x, y) {
+    return x + y;
+}
 
 // https://www.geeksforgeeks.org/subtract-two-numbers-without-using-arithmetic-operators/
 module.exports.minus = (x, y) => {
